a7dee66bd8059438e1ecc09b3f4c1716e0ac63eb
Marin Koštić <kosticmarin@gmail.com>
2019-05-28 22:12:11 +0200

    Updates

---
+++ precache-manifest.1b9641a8bdab573c2563b8c11c71dca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d259c075aac75c0f343be565bd68359b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f013ccbe811d2a385d5",
+    "url": "/static/css/main.39924d42.chunk.css"
+  },
+  {
+    "revision": "7db911faebd2f3500565",
+    "url": "/static/js/2.479fc036.chunk.js"
+  },
+  {
+    "revision": "5f013ccbe811d2a385d5",
+    "url": "/static/js/main.e2cac2bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
