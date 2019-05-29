d1c5e83a96b3efcadfe614622178c0292a908898
Marin Koštić <kosticmarin@gmail.com>
2019-05-29 07:45:12 +0200

    Updates

--- precache-manifest.8a748d893d66e3db9c3936b5fd1f9a24.js
+++ precache-manifest.8a748d893d66e3db9c3936b5fd1f9a24.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3eef64068549550a6b293d562eca152e",
+    "revision": "2daef2fd4d6dfe20581fb2a2a0b89b37",
     "url": "/index.html"
   },
   {
-    "revision": "b1c8ae48a2cb83dfd1a2",
-    "url": "/static/css/main.4353d59f.chunk.css"
+    "revision": "ca503ee8e991231978a1",
+    "url": "/static/css/main.1dac686c.chunk.css"
   },
   {
     "revision": "7db911faebd2f3500565",
     "url": "/static/js/2.479fc036.chunk.js"
   },
   {
-    "revision": "b1c8ae48a2cb83dfd1a2",
+    "revision": "ca503ee8e991231978a1",
     "url": "/static/js/main.e2cac2bb.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e90c9f004ddb73ff1259a0dafbc96e8.js"
+  "/precache-manifest.8a748d893d66e3db9c3936b5fd1f9a24.js"
 );
 
 self.addEventListener('message', (event) => {
